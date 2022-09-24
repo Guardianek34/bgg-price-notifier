6b29a535ac203af9b370125166d3266185c6f154
Guardianek34 <guardianek34@gmail.com>
2022-09-24 15:05:41 +0200

    Initial commit

---
+++ src/test/java/com/example/bggpricenotifier/BggPriceNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bggpricenotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BggPriceNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
